f1d1a31723df36a685a13deba2315504026caca9
j-snowden98 <32417992+j-snowden98@users.noreply.github.com>
2020-05-05 15:27:30 +0100

    Improved email validation on RFC 5322 standard. Also made contacts a uni-directional relationship

--- src/main/java/joe/entwa/ent/Account.java
+++ src/main/java/joe/entwa/ent/Account.java
@@ -40,14 +40,12 @@ public class Account implements Serializable {
     private String county;
     private String postcode;
     private String telephone;
-    @Pattern(regexp = "[a-zA-Z0-9.]+@[a-zA-Z0-9]+[.a-zA-Z0-9]+", message = "Email format is invalid.")
+    
+    @Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", message = "Email format is invalid.")
     private String email;
     
     @ManyToMany 
     private List<Account> contacts = new ArrayList<>();
-    
-    @ManyToMany(mappedBy="contacts")
-    private List<Account> contactsOf = new ArrayList<>();
     
     @OneToMany(mappedBy="owner")
     private List<Appointment> ownedAppointments = new ArrayList<>();
@@ -175,15 +173,7 @@ public class Account implements Serializable {
     public void setContacts(List<Account> contacts) {
         this.contacts = contacts;
     }
-
-    public List<Account> getContactsOf() {
-        return contactsOf;
-    }
-
-    public void setContactsOf(List<Account> contactsOf) {
-        this.contactsOf = contactsOf;
-    }
-
+    
     public List<Appointment> getOwnedAppointments() {
         return ownedAppointments;
     }
