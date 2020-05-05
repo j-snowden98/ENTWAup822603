da37f5289aa48320ea3c1f8c151851faa04dcdf8
j-snowden98 <32417992+j-snowden98@users.noreply.github.com>
2020-05-05 17:09:34 +0100

    Added postcode validation to register user

--- src/main/java/joe/entwa/ent/Account.java
+++ src/main/java/joe/entwa/ent/Account.java
@@ -38,6 +38,7 @@ public class Account implements Serializable {
     private String address2;
     private String town;
     private String county;
+    @Pattern(regexp="^$|^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$", message = "Postcode is invalid")
     private String postcode;
     
     @Pattern(regexp="\\+44\\s*\\(\\d{2}\\)\\s*\\d{8}|\\(\\d{3}\\)\\s*\\d{8}|^$", message = "Telephone number is invalid")
