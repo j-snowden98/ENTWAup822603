d485316f10e28117ae281f6c70265c1e168093a2
j-snowden98 <32417992+j-snowden98@users.noreply.github.com>
2020-05-05 17:02:22 +0100

    Validation of telephone number added

--- src/main/java/joe/entwa/ent/Account.java
+++ src/main/java/joe/entwa/ent/Account.java
@@ -18,7 +18,7 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 /**
- *
+ * Entity class for representing user accounts in the system.
  * @author Joe
  */
 @Entity
@@ -39,6 +39,8 @@ public class Account implements Serializable {
     private String town;
     private String county;
     private String postcode;
+    
+    @Pattern(regexp="\\+44\\s*\\(\\d{2}\\)\\s*\\d{8}|\\(\\d{3}\\)\\s*\\d{8}|^$", message = "Telephone number is invalid")
     private String telephone;
     
     @Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", message = "Email format is invalid.")
