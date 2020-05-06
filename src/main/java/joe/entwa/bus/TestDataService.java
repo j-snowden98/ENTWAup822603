4c06dd58b5f9494d885a37738e314294f8350293
j-snowden98 <32417992+j-snowden98@users.noreply.github.com>
2020-05-06 16:57:09 +0100

    Test data telephone and postcodes set to valid formats

--- src/main/java/joe/entwa/bus/TestDataService.java
+++ src/main/java/joe/entwa/bus/TestDataService.java
@@ -31,34 +31,34 @@ public class TestDataService {
     AppointmentFacade app;
     
     public void insertTestData() {
-        Account user1 = new Account("Becky", "Draper", "user1", "password1", "1 London Road", "North end", "Portsmouth", "Hampshire", "PO2 1AA", "07123456789", "user1@google.com");
+        Account user1 = new Account("Becky", "Draper", "user1", "password1", "1 London Road", "North end", "Portsmouth", "Hampshire", "PO2 8PJ", "+44(71)23456789", "user1@google.com");
         acc.create(user1);
         
-        Account user2 = new Account("Kloe", "Duarte", "user2", "password2", "2 London Road", "North end", "Portsmouth", "Hampshire", "PO2 1AB", "07133456789", "user2@google.com");
+        Account user2 = new Account("Kloe", "Duarte", "user2", "password2", "2 London Road", "North end", "Portsmouth", "Hampshire", "PO2 8PJ", "(071)33456789", "user2@google.com");
         acc.create(user2);
         
-        Account user3 = new Account("Bryson", "Sharma", "user3", "password3", "3 London Road", "North end", "Portsmouth", "Hampshire", "PO2 1AC", "07123776789", "user3@google.com");
+        Account user3 = new Account("Bryson", "Sharma", "user3", "password3", "3 London Road", "North end", "Portsmouth", "Hampshire", "PO2 8PJ", "(071)23776789", "user3@google.com");
         acc.create(user3);
         
-        Account user4 = new Account("Drake", "Hutton", "user4", "password4", "4 London Road", "North end", "Portsmouth", "Hampshire", "PO2 1AD", "07122656789", "user4@google.com");
+        Account user4 = new Account("Drake", "Hutton", "user4", "password4", "4 London Road", "North end", "Portsmouth", "Hampshire", "PO2 8PJ", "+44(71)22656789", "user4@google.com");
         acc.create(user4);
         
-        Account user5 = new Account("Mackenzie", "Manning", "user5", "password5", "5 London Road", "North end", "Portsmouth", "Hampshire", "PO2 1AE", "07123456679", "user5@google.com");
+        Account user5 = new Account("Mackenzie", "Manning", "user5", "password5", "5 London Road", "North end", "Portsmouth", "Hampshire", "PO2 8PJ", "(071)23456679", "user5@google.com");
         acc.create(user5);
         
-        Account user6 = new Account("Colette", "Kramer", "user6", "password6", "6 London Road", "North end", "Portsmouth", "Hampshire", "PO2 1AF", "07123461789", "user6@google.com");
+        Account user6 = new Account("Colette", "Kramer", "user6", "password6", "6 London Road", "North end", "Portsmouth", "Hampshire", "PO2 8PJ", "+44(71)23461789", "user6@google.com");
         acc.create(user6);
         
-        Account user7 = new Account("Safia", "Kinney", "user7", "password7", "7 London Road", "North end", "Portsmouth", "Hampshire", "PO2 1AG", "07124984789", "user7@google.com");
+        Account user7 = new Account("Safia", "Kinney", "user7", "password7", "7 London Road", "North end", "Portsmouth", "Hampshire", "PO2 8PJ", "+44 (71)24984789", "user7@google.com");
         acc.create(user7);
         
-        Account user8 = new Account("Alberto", "Doyle", "user8", "password8", "8 London Road", "North end", "Portsmouth", "Hampshire", "PO2 1AH", "07123462829", "user8@google.com");
+        Account user8 = new Account("Alberto", "Doyle", "user8", "password8", "8 London Road", "North end", "Portsmouth", "Hampshire", "PO2 8PJ", "(071) 23462829", "user8@google.com");
         acc.create(user8);
         
-        Account user9 = new Account("Abbie", "Faufman", "user9", "password9", "9 London Road", "North end", "Portsmouth", "Hampshire", "PO2 1AI", "07123474789", "user9@google.com");
+        Account user9 = new Account("Abbie", "Faufman", "user9", "password9", "9 London Road", "North end", "Portsmouth", "Hampshire", "PO2 8PJ", "(071)23474789", "user9@google.com");
         acc.create(user9);
         
-        Account user10 = new Account("Jesse", "Howarth", "user10", "password10", "10 London Road", "North end", "Portsmouth", "Hampshire", "PO2 1AJ", "07123967789", "user10@google.com");
+        Account user10 = new Account("Jesse", "Howarth", "user10", "password10", "10 London Road", "North end", "Portsmouth", "Hampshire", "PO2 8PJ", "(071)23967789", "user10@google.com");
         acc.create(user10);
         
         user1.getContacts().add(user2);
